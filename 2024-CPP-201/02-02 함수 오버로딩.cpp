7199045051ab052f9d55308227c229a812d3ae11
soomin <s2305e-mirim.hs.kr>
2024-03-18 15:05:30 +0900

    함수 오버로당 적용

--- 2024-CPP-201/02-02 함수 오버로딩.cpp
+++ 2024-CPP-201/02-02 함수 오버로딩.cpp
@@ -2,15 +2,15 @@
 
 using namespace std;
 
-// 전통적인 C언어는 함수명이 중복될 수 없다.
-int sum_int(int a, int b) {
+// 함수 오버로딩을 사용하면 함수이름을 짓는데 부담을 줄일 수 있다.
+int sum(int a, int b) {
 	return a + b;
 }
 
-float sum_float(float a, float b) {
+float sum(float a, float b) {
 	return a + b;
 }
 
 void main(void)	{
-	cout << sum_int(1, 2) << sum_float(1.1f, 2.2f) << endl;
+	cout << sum(1, 2) << sum(1.1f, 2.2f) << endl;
 }
