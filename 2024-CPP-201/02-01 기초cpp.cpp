5802b35ff9eda5ab9e64a24eb871065f895ccc36
soomin <s2305e-mirim.hs.kr>
2024-03-18 14:27:59 +0900

    c++ 기본형태

---
+++ 2024-CPP-201/02-01 기초cpp.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+using namespace std;		// 더이상 std::를 안써도 된다
+
+void main(void) {
+	// std : 이름공간(name space)
+	// cout : 출력 객체
+	// endl : 줄바꿈(개행) 객체
+	std::cout << "Hello World" << std::endl;
+
+}
