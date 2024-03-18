f1c2bf3d61cbced953da5b0703e15f06eba2aeda
soomin <s2305e-mirim.hs.kr>
2024-03-18 14:32:57 +0900

    namespace

--- 2024-CPP-201/02-01 기초cpp.cpp
+++ 2024-CPP-201/02-01 기초cpp.cpp
@@ -2,9 +2,8 @@
 
 using namespace std;		// 더이상 std::를 안써도 된다
 
-void main(void)
-{
-	// std : 이름공간(name space)
+void main(void)	{
+	// std : 이름공간(name space) 변수의 중복을 막기 위함
 	// cout : 출력 객체
 	// endl : 줄바꿈(개행) 객체
 	std::cout << "Hello World" << std::endl;
