31c23265c7ccf6fd05e672a57518aee9a684eb73
soomin <s2305e-mirim.hs.kr>
2024-03-18 15:02:48 +0900

    함수 오버로딩이 안될 때

---
+++ 2024-CPP-201/02-02 함수 오버로딩.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// 전통적인 C언어는 함수명이 중복될 수 없다.
+int sum_int(int a, int b) {
+	return a + b;
+}
+
+float sum_float(float a, float b) {
+	return a + b;
+}
+
+void main(void)	{
+	cout << sum_int(1, 2) << sum_float(1.1f, 2.2f) << endl;
+}
