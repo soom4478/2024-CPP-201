9488774bb25c337c880467cd660e581bf1f238ba
soomin <s2305e-mirim.hs.kr>
2024-03-25 14:32:59 +0900

    call by porinter

--- 2024-CPP-201/Call-by.cpp
+++ 2024-CPP-201/Call-by.cpp
@@ -2,17 +2,16 @@
 
 using namespace std;
 
-void call_by_value(int a) {
-	// 새로운 지역변수 a를 만들고 거기에 값을 넣는다.
-	a = 10;
-
+void call_by_value(int* a) {
+	// 새로운 지역변수 a를 만들고 a가 가리키고 있는 대상의 값을 바꾼다
+	*a = 10;
 }
 
 int main(void) {
 	int num = 3;
 
-	// num의 값이 바끼지 않는다.
-	call_by_value(num);
+	// num의 값이 바뀐다
+	call_by_value(&num);
 	cout << num;
 	return 0;
 }
