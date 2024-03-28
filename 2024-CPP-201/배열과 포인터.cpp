362e3483ff77decdb9a91b4ad13688b465fa03c3
soomin <s2305e-mirim.hs.kr>
2024-03-29 08:53:23 +0900

    arr[i]에서 [i]에는 *연산이 들어있다

--- 2024-CPP-201/배열과 포인터.cpp
+++ 2024-CPP-201/배열과 포인터.cpp
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-	int arr[4];
+	int arr[4] = {10,20,30,40};
 	char str[4];
 
-	// 4바이트 차이(x86으로 설정하면 더 좋음)
-	printf("%d %d\n", &arr[0], &arr[1]);
-	// 위의 코드와 결과가 같다
-	printf("%d %d\n", arr, arr+1);
+	// 두 코드의 결과는 같다
+	printf("%d %d\n", *(&arr[0]), *(&arr[1]));
+	printf("%d %d\n", *(arr), *(arr+1));
 
 	return 0;
 }
