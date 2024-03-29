2b5c3fe7482b2a32cd2b428cbc04df0dc7261230
soomin <s2305e-mirim.hs.kr>
2024-03-29 10:20:09 +0900

    배열과 포인터 그리고 함수

---
+++ 2024-CPP-201/배열과 포인터와 함수.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// 배열의 모든 원소를 0으로 한다
+void erase(int* _arr, int _length) {
+	for (int i = 0; i < _length; i++) {
+		_arr[i] = 0;
+	}
+}
+
+int main(void) {
+	int arr[4] = {10,20,30,40};
+	int length = sizeof(arr) / sizeof(arr[0]);
+	erase(arr, length);
+
+	for (int i = 0; i < 4; i++) {
+		printf("%d ", arr[i]);
+	}
+
+	return 0;
+}
