f5e7cf75ade097f3029427e59be2099115a8a776
soomin <s2305e-mirim.hs.kr>
2024-03-29 09:39:59 +0900

    포인터는 배열처럼 사용할 수 있다

--- 2024-CPP-201/배열과 포인터.cpp
+++ 2024-CPP-201/배열과 포인터.cpp
@@ -2,17 +2,19 @@
 
 int main(void) {
 	int arr[4] = {10,20,30,40};
-	char str[4];
+	char str[4] = "swa";
 
 	int* pi = arr;
 	char* pc = str;
 
 	// 두 코드의 결과는 같다
-	printf("%d %d\n", (&arr[0]), (&arr[1]));	// 4차이
-	printf("%d %d\n\n", (arr), (arr+1));
+	printf("%d %d\n", arr[0], arr[1]);	// 4차이
+	printf("%d %d\n\n", *(arr), *(arr+1));
 
-	printf("%d %d\n", (pi), (pi + 1));	// 4차이
-	printf("%d %d\n", (pc), (pc + 1));	// 1차이
+	printf("%d %d\n", *(pi), *(pi + 1));
+	printf("%d %d\n", pi[0], pi[1]);
+	printf("%c %c\n", *(pc), *(pc + 1));
+	printf("%c %c\n", pc[0], pc[1]);
 
 	return 0;
 }
