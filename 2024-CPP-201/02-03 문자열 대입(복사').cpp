5671bacf13e6635b72a7f42ff14acf56276333f4
soomin <s2305@e-mirim.hs.kr>
2024-04-05 09:41:08 +0900

    문자열 대입(복사)-C스타일2

--- 2024-CPP-201/02-03 문자열 대입(복사').cpp
+++ 2024-CPP-201/02-03 문자열 대입(복사').cpp
@@ -1,17 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
 
 	char str[100] = "Juyoung";
 	char str2[100];
 
-	int i = 0;
 	// str에 있는 내용을 str2에 복사하기
-	for (; i < str[i] != '\0'; i++) {
-		str2[i] = str[i];
-	}
-
-	str2[i] = str[i];	// '\0'
+	strcpy(str2, str);
 
 	printf("%s", str2);
 }
