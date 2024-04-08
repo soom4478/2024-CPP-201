ad79c23fbe763fe8f3e373d59d26f1e5abf9eb85
soomin <s2305@e-mirim.hs.kr>
2024-04-08 13:42:37 +0900

    문자열 비교-C++스타일

---
+++ 2024-CPP-201/02-05 문자열 비교.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	string str = "Juyoung";
+	// 문자열 비교
+	if (str == "Juyoung")
+		cout << "같다";
+	else
+		cout << "다르다";
+}
