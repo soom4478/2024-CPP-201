78f377044e1f142c8435e433940d841ecc532b64
soomin <s2305@e-mirim.hs.kr>
2024-04-08 14:59:55 +0900

    생성자

--- 2024-CPP-201/03-01 구조체와 클래스.cpp
+++ 2024-CPP-201/03-01 구조체와 클래스.cpp
@@ -4,13 +4,20 @@ using namespace std;
 
 // 클래스는 디폴트가 private
 class Student {
+public:
+	Student(int hakbun, string name) {
+		hakbun_ = hakbun;
+		name_ = name;
+	}
+private:
+	// 생성자 : 객체를 생성할 때 호출되는 함수
+	// 반환형이 없다.
 	int hakbun_;
 	string name_;
 };
 
 void main(void) {
-	struct Student juyoung;
-
-	juyoung.hakbun_ = 2115;	// 에러
-	juyoung.name_ = "윤주영";
+	// 매개변수가 없는 생성자 호출
+	// Student juyoung = Student();
+	Student juyoung = Student(2115, "윤주영");
 }
