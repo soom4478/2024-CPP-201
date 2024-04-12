5a04f79f75a8c0d775ce8e47e8a748589d2fa338
soomin <s2305@e-mirim.hs.kr>
2024-04-12 09:44:54 +0900

    멤버함수 초기화

--- 2024-CPP-201/03-01 구조체와 클래스.cpp
+++ 2024-CPP-201/03-01 구조체와 클래스.cpp
@@ -7,15 +7,15 @@ class Student {
 public:
 	// 생성자 : 객체를 생성할 때 호출되는 함수
 	// 반환형이 없다.
-	Student(int hakbun, string name) {
-		hakbun_ = hakbun;
-		name_ = name;
+	Student(int hakbun, string name)
+		: hakbun_(hakbun), name_(name)	// 초기화를 동시에 (const를 할 수 있다)
+	{
 	}
 
 	// 생성자 오버로딩
-	Student() {
-		hakbun_ = 2121;
-		name_ = "JWP";
+	Student() 
+		: hakbun_(2121), name_("JWP")
+	{
 	}
 
 	// 객체의 멤버를 출력하는 함수
