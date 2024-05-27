77ec3e79cf67f428155ab2eca6d5f0fe0756ff28
soomin <s2305@e-mirim.hs.kr>
2024-05-27 15:05:51 +0900

    05-01 부모/자식 생성/소멸자 호출 순서

---
+++ 2024-CPP-201/05-01 상속 기본.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+public:
+	Person(string name, int age)
+		: name_(name), age_(age) 
+	{
+		cout << "부모생성자 호출" << endl;
+	}
+	~Person()
+	{
+		cout << "부모소멸자 호출" << endl;
+	}
+	void show_person() {
+		cout << name_ << "의 나이는 " << age_ << "입니다." << endl;
+	}
+	
+private:
+	string name_;
+	int age_;
+};
+// Person의 자식(파생)클래스
+class Student : public Person {
+public:
+	// 부모(기본)클래스의 생성자가 먼저 호출되어야 한다
+	Student(int id, string name, int age) : Person(name, age), id_(id)
+	{
+		cout << "자식생성자 호출" << endl;
+	}
+	~Student()
+	{
+		cout << "자식소멸자 호출" << endl;
+	}
+	void show_person(void) {
+		cout << "학번" << id_ << endl;
+		Person::show_person();
+	}
+private:
+	int id_;
+};
+
+int main(void) {
+	Student* s = new Student(2100, "이름", 18);
+
+	s->show_person();
+
+	delete s;
+	return 0;
+}
---
+++ 2024-CPP-201/gyosengssem.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal {
+    string name;
+    int weight;
+
+public:
+    // 기본 생성자
+    Animal() { // 기본값으로 초기화
+    }
+
+    // 매개변수 생성자
+    Animal(const string name, int weight) : name(name), weight(weight) { // 멤버 초기화
+        cout << " Animal 부모 클래스" << endl;
+    }
+
+    ~Animal() {
+        cout << " Animal 소멸 " << endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    // 매개변수 생성자
+    Cat(const string name, int weight)
+        : Animal(name, weight) // Animal 클래스의 매개변수 생성자 호출
+    {
+        cout << " Cat 생성 " << endl;
+    }
+    ~Cat() {
+        cout << " Cat 소멸 " << endl;
+    }
+};
+
+// `main` 함수 정의
+int main() {
+    // Cat 객체 생성
+    Cat cat("미림고양이", 55); // 이때 Animal의 매개변수 생성자가 호출됨
+    return 0;
+}
