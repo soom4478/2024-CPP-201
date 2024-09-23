da4d5b70dd5666f2ea0b6d79e121b4666fd91140
soomin <s2305@e-mirim.hs.kr>
2024-09-23 15:04:59 +0900

    08-02 map

--- 2024-CPP-201/08-01 vector.cpp
+++ 2024-CPP-201/08-01 vector.cpp
@@ -1,42 +1,33 @@
 #include <iostream>
-#include <vector>
+#include <map>
 #include <string>
 
 using namespace std;
 
 void main(void) {
-	vector<string> cy; // 동적배열 : size변화 가능
 
-	// 미리 벡터의 크기를 지정하는 것이 좋다
-	cy.reserve(100);
+	map<string, int> guardian;
 	
+
 	// 요소 추가
-	cy.push_back("채연");
-	cy.push_back("해인");
-	cy.push_back("우석");
-	cy.push_back("덱스");
-	cy.push_back("서준");
-	cy.push_back("정국");
+	guardian["김신"] = 939;
+	guardian["지은탁"] = 19;
+	guardian["저승사자"] = 300;
+	guardian["써니"] = 29;
+	guardian["유덕화"] = 25;
 
 	// 요소 접근
-	cout << cy[0] << ", " << cy.at(1) << endl;
-	cout << cy[0] << ", " << cy.at(2) << endl;
+	cout << "김신의 나이 : " << guardian["김신"] << endl;
+	cout << "저승사자의 나이 : " << guardian["저승사자"] << endl;
 
 	// 요소 수정
-	cy[1] = "석진";
+	guardian["지은탁"] = 29;
 
 	// 요소 갯수
-	cout << cy[0] << "의 남자들★★★★★" << endl;
-	for (int i = 1; i < cy.size(); i++)
-		cout << cy[i] << endl;
+	cout << guardian.size() << endl;
 
-	// 마지막 요소 삭제
-	cy.pop_back();
-	cout << cy[0] << "의 남자들★★★★★" << endl;
-	for (int i = 1; i < cy.size(); i++)
-		cout << cy[i] << endl;
-
-	vector<string>::iterator iter;
-	for (iter = cy.begin(); iter!= cy.end(); iter++)
-		cout << *iter << endl;
+	// 모든 요소 출력(first:key, second:value)
+	map<string, int>::iterator iter;
+	for (iter = guardian.begin(); iter!= guardian.end(); iter++)
+		cout << iter->first << "는 " << iter->second << "살" << endl;
 }
