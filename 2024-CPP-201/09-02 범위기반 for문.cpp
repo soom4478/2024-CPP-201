2fcaec14beec8380f73a719a5ffff765ab19c383
soomin <s2305@e-mirim.hs.kr>
2024-10-14 13:49:02 +0900

    09-04 auto

--- 2024-CPP-201/09-02 범위기반 for문.cpp
+++ 2024-CPP-201/09-02 범위기반 for문.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 void main(void)
 {
-    int a = 40;
+    auto a = 40;
     cout << a << endl;
 
     vector<string> v = { "±èÁöÇı", "½ÉÁ¤¹Î", "À±ÁÖ¿µ" };
-    string b = v[2];
+    auto b = v[2];
     cout << b << endl;
     
 }
