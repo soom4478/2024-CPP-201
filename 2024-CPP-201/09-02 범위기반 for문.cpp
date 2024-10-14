31c0ced81cbe48bf6c7212292c2ec4289885addd
soomin <s2305@e-mirim.hs.kr>
2024-10-14 13:40:51 +0900

    09-03 auto 적용 전

--- 2024-CPP-201/09-02 범위기반 for문.cpp
+++ 2024-CPP-201/09-02 범위기반 for문.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
-void main(void) {
-    vector<int> arr{ 201, 202, 203, 204 };
-    for (int x : arr) {
-        cout << x << " ";
-    }
+void main(void)
+{
+    int a = 40;
+    cout << a << endl;
+
+    vector<string> v = { "±èÁöÇı", "½ÉÁ¤¹Î", "À±ÁÖ¿µ" };
+    string b = v[2];
+    cout << b << endl;
     
 }
