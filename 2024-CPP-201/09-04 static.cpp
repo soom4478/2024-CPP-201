8e2e85916ac5779d49e9bca1d465fd639190bcaa
soomin <s2305@e-mirim.hs.kr>
2024-10-14 15:08:15 +0900

    09-04 static 멤버변수

---
+++ 2024-CPP-201/09-04 static.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class PrettyJ {
+private:
+	static int num_;
+	int gam_;
+	string name_;
+
+public:
+	PrettyJ(int gam, string name) : gam_(gam), name_(name) { num_++; }
+	static int get_num(void) { return num_; }
+	int get_gam(void) { return gam_; }
+	int* get_address_gam(void) { return &gam_;}
+	int* get_address_num(void) { return &num_; }
+};
+// static 멤버변수 초기화
+int PrettyJ::num_ = 0;
+
+void main(void) {
+	PrettyJ* jihye = new PrettyJ(508, "지혜");
+	PrettyJ* juyoung = new PrettyJ(509, "주영");
+	PrettyJ* jungmin = new PrettyJ(716, "정민");
+	cout << "현재 멤버 인원은 : " << PrettyJ::get_num() << endl;
+	PrettyJ* jiwoo = new PrettyJ(601, "지우");
+	cout << "현재 멤버 인원은 : " << PrettyJ::get_num() << endl;
+
+	cout << "지혜 gam의 주소" << jihye->get_address_gam() << endl;
+	cout << "주영 gam의 주소" << juyoung->get_address_gam() << endl;
+	cout << "정민 gam의 주소" << jungmin->get_address_gam() << endl;
+	cout << "지우 gam의 주소" << jiwoo->get_address_gam() << endl << endl;
+
+	cout << "지혜 num의 주소" << jihye->get_address_num() << endl;
+	cout << "주영 num의 주소" << juyoung->get_address_num() << endl;
+	cout << "정민 num의 주소" << jungmin->get_address_num() << endl;
+	cout << "지우 num의 주소" << jiwoo->get_address_num() << endl << endl;
+
+
+	delete jiwoo;
+	delete jungmin;
+	delete juyoung;
+	delete jihye;
+}
