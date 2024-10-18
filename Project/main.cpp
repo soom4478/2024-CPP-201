af22c5ab6ba98c7d2f1d257754775b5f8fce10af
soomin <s2305@e-mirim.hs.kr>
2024-10-18 09:56:56 +0900

    [pj] 상수 처리방법1

--- Project/main.cpp
+++ Project/main.cpp
@@ -3,8 +3,11 @@
 using namespace sf;
 using namespace std;
 
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 800
+
 void main(void) {
-	RenderWindow window(VideoMode(1200, 800), "Project");
+	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Project");
 
 	while (window.isOpen()) {
 		Event event;
