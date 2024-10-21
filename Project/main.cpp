0a007bd06f39d1778bb4b8b780f675c9e2b8f5ec
soomin <s2305@e-mirim.hs.kr>
2024-10-21 13:50:22 +0900

    player 이동

--- Project/main.cpp
+++ Project/main.cpp
@@ -11,6 +11,11 @@ enum App {
 void main(void) {
 	RenderWindow window(VideoMode(App::WIDTH, App::HEIGHT), "Project");
 
+	RectangleShape player;
+	player.setSize(Vector2f(200.f, 200.f));
+	player.setPosition((App::WIDTH - player.getSize().x) / 2, (App::HEIGHT - player.getSize().y) / 2);
+	player.setFillColor(Color::Magenta);
+
 	while (window.isOpen()) {
 		Event event;
 		while (window.pollEvent(event)) {
@@ -18,10 +23,14 @@ void main(void) {
 				window.close();
 		}
 
-		RectangleShape player;
-		player.setSize(Vector2f(200.f, 200.f));
-		player.setPosition((App::WIDTH-player.getSize().x)/2, (App::HEIGHT - player.getSize().y) / 2);
-		player.setFillColor(Color::Magenta);
+		if (Keyboard::isKeyPressed(Keyboard::Key::Right))
+			player.move(10.f, 0.f);
+		if (Keyboard::isKeyPressed(Keyboard::Key::Left))
+			player.move(-10.f, 0.f);
+		if (Keyboard::isKeyPressed(Keyboard::Key::Up))
+			player.move(0.f, 10.f);
+		if (Keyboard::isKeyPressed(Keyboard::Key::Down))
+			player.move(0.f, -10.f);
 
 		window.clear(Color::White);
 		window.draw(player);
